807efe9a67d2acdea301c5dd985b362d0b726deb
Marcoatisabel <orsacchiotto80@hotmail.com>
2025-06-24 13:57:47 +0200

    sass, get rid of extra css, scss, glow for the home page

--- app.vue
+++ app.vue
@@ -3,8 +3,7 @@
 </template>
 
 <script setup>
-import '~/assets/tailwind.css'
-import '~/assets/style.css'
+import '~/assets/style.scss'
 import { useHead } from '#imports'
 
 useHead({
@@ -15,14 +14,14 @@ useHead({
   ],
   link: [
     { rel: 'icon', type: 'image/svg+xml', href: '/assets/favicon.svg' },
-    { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' },
     { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
     { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
     { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap' },
     { rel: 'stylesheet', href: 'https://unpkg.com/swiper/swiper-bundle.min.css' }
   ],
   script: [
-    { src: 'https://unpkg.com/swiper/swiper-bundle.min.js', body: true }
+    { src: 'https://unpkg.com/swiper/swiper-bundle.min.js', body: true },
+    { src: 'https://kit.fontawesome.com/960a8c2498.js', crossorigin: 'anonymous' }
   ]
 })
 </script>
--- components/hero/HeroIndex.vue
+++ components/hero/HeroIndex.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- Hero Section -->
-  <section class="w-full h-[100vh] bg-[#161616] text-white relative overflow-hidden flex items-center justify-center hero-section">
+  <section class="w-full h-[100vh] bg-[#161616] text-white relative overflow-hidden flex items-center justify-center hero-section group">
+    <GlowEffect />
     <div class="max-w-6xl mx-auto text-center px-6 relative z-10 w-full">
       <h1 class="text-4xl sm:text-5xl font-extrabold mb-6">{{ title }}</h1>
       <h2 class="text-xl sm:text-2xl font-medium mb-8">{{ subtitle }}</h2>
