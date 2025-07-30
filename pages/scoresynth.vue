9ea6b766107b509cd118fe6fe3897376c0c325c6
Marcoatisabel <orsacchiotto80@hotmail.com>
2025-07-30 16:56:47 +0200

    Fix image filename case and update reference
    Renamed 'UI'sOverPattern.png' to 'UiOverPattern.png' to correct casing and updated the reference in scoresynth.vue to match the new filename.

--- pages/scoresynth.vue
+++ pages/scoresynth.vue
@@ -42,7 +42,7 @@ useHead({
         { tag: 'p', text: 'Whether you’re humming a melody, exporting a MIDI, or uploading a full demo track, ScoreSynth transforms your input into a professionally arranged orchestral score. Heres how:', margin: 'mb-0' }
     ]" />
 
-    <OneColumnImage glowEffect="GlowRed" centerImg="/assets/new-assets/products-page/product/score-synth/how-works/UI'sOverPattern.png" centerImgAlt="ScoreSynth UI Interface" />
+    <OneColumnImage glowEffect="GlowRed" centerImg="/assets/new-assets/products-page/product/score-synth/how-works/UiOverPattern.png" centerImgAlt="ScoreSynth UI Interface" />
 
     <ProcessSection accentColor="text-[#AA3733]" imageSrc="/assets/why-1.png" imageClass="px-10 py-10" imageAlt="Synth"
         :images="[
