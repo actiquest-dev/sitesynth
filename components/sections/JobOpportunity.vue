1602d00ab5b3f2a913a56b901267f83475184c77
Marcoatisabel <orsacchiotto80@hotmail.com>
2025-09-21 13:47:11 +0200

    Refactor job box to use anchor for navigation
    Replaces the click handler with an anchor tag for job navigation, improving accessibility and semantic HTML. Removes the handleJobClick method and ensures the entire job box is wrapped in a link.

--- components/sections/JobOpportunity.vue
+++ components/sections/JobOpportunity.vue
@@ -7,47 +7,46 @@
           :key="index"
           :class="[job.bgColor || '']"
           class="job-box group relative border border-[#636363] flex cursor-pointer"
-          @click="handleJobClick(job.link)"
         >
-          <!-- Image Section (1/6 width) -->
-          <div class="w-1/6 flex-shrink-0">
-            <img
-              :src="job.image"
-              :alt="job.imageAlt || job.title"
-              class="w-full h-full object-cover"
-            />
-          </div>
+          <a :href="job.link" class="flex w-full" rel="noopener noreferrer">
+            <!-- Image Section (1/6 width) -->
+            <div class="w-1/6 flex-shrink-0">
+              <img
+                :src="job.image"
+                :alt="job.imageAlt || job.title"
+                class="w-full h-full object-cover"
+              />
+            </div>
 
-          <!-- Content Section (4/6 width) -->
-          <div class="w-4/6 p-6 flex flex-col justify-center">
-            <!-- Title -->
-            <component :is="job.titleTag || 'h3'" :class="job.titleClass">
-              {{ job.title }}
-            </component>
+            <!-- Content Section (4/6 width) -->
+            <div class="w-4/6 p-6 flex flex-col justify-center">
+              <!-- Title -->
+              <component :is="job.titleTag || 'h3'" :class="job.titleClass">
+                {{ job.title }}
+              </component>
 
-            <component
-              :is="job.descriptionClass ? 'p' : 'div'"
-              :class="job.descriptionClass"
-            >
-              {{ job.description }}
-            </component>
-          </div>
+              <component
+                :is="job.descriptionClass ? 'p' : 'div'"
+                :class="job.descriptionClass"
+              >
+                {{ job.description }}
+              </component>
+            </div>
 
-          <!-- Link Section (1/6 width) -->
-          <div class="w-1/6 flex flex-col justify-center items-center">
-            <a
-              :href="job.link"
-              class="text-[#636363] flex items-center group-hover:text-white transition-colors duration-200"
-              @click.stop
-            >
-              <span class="mr-2 font-semibold">
-                {{ job.linkText }}
+            <!-- Link Section (1/6 width) -->
+            <div class="w-1/6 flex flex-col justify-center items-center">
+              <span
+                class="text-[#636363] flex items-center group-hover:text-white transition-colors duration-200"
+              >
+                <span class="mr-2 font-semibold">
+                  {{ job.linkText }}
+                </span>
+                <i
+                  class="fa fa-chevron-right group-hover:translate-x-1 transition-transform duration-200"
+                ></i>
               </span>
-              <i
-                class="fa fa-chevron-right group-hover:translate-x-1 transition-transform duration-200"
-              ></i>
-            </a>
-          </div>
+            </div>
+          </a>
         </div>
       </div>
     </div>
@@ -69,10 +68,4 @@ const props = defineProps({
     required: true,
   },
 });
-
-const handleJobClick = (link) => {
-  if (link) {
-    window.open(link, "_blank");
-  }
-};
 </script>
