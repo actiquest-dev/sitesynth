2b297361dd4b5ee195191578a943e975521bd394
sitesynth <hello@sitesynth.com>
2025-11-14 01:17:26 +0100

    Update Testimonial.vue

--- components/ui/Testimonial.vue
+++ components/ui/Testimonial.vue
@@ -5,7 +5,7 @@
         ref="card"
         class="grid grid-cols-1 md:grid-cols-3 gap-8 relative border border-[#636363] py-14 group overflow-hidden"
       >
-        <component :is="selectedGlowEffect" />
+        <component :is="selectedGlowEffect" class="absolute inset-0 z-0 pointer-events-none" />
         <!-- Left Column (1/3) -->
         <div class="flex flex-col items-center text-center">
           <img
@@ -17,7 +17,7 @@
           <p class="text-gray-300 mt-2">{{ personTitle }}</p>
         </div>
         <!-- Right Column (2/3) -->
-        <div class="md:col-span-2 flex flex-col justify-center px-6">
+       <div class="md:col-span-2 flex flex-col justify-center px-6 relative z-10">
           <i class="text-6xl fa-solid fa-quote-left" :class="quoteColor"></i>
           <h3 class="text-4xl font-black text-white mt-4 mb-6">
             {{ quoteTitle }}
