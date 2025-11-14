3fa4fae8489868fd48e0b1eb986bdeed70e03566
sitesynth <hello@sitesynth.com>
2025-11-14 01:07:56 +0100

    Update Testimonial.vue white

--- components/ui/Testimonial.vue
+++ components/ui/Testimonial.vue
@@ -1,32 +1,28 @@
 <template>
-  <section :id="id" class="bg-[#161616] flex justify-center">
+  <section :id="id" class="bg-[#DDDDDD] flex justify-center">
     <div class="max-w-[1248px] mx-auto px-6">
       <div
         ref="card"
-        class="grid grid-cols-1 md:grid-cols-2 gap-12 relative border border-[#636363] p-12 group overflow-hidden"
+        class="grid grid-cols-1 md:grid-cols-3 gap-8 relative border border-[#636363] py-14 group overflow-hidden"
       >
-        <component :is="selectedGlowEffect" />
-        
-        <!-- Left Column - Profile -->
-        <div class="flex flex-col items-center justify-center text-center relative z-10">
+        <GlowEffect />
+        <!-- Left Column (1/3) -->
+        <div class="flex flex-col items-center text-center">
           <img
             :src="imageSrc"
             :alt="personName"
-            class="mb-6 w-32 h-32 rounded-full object-cover"
+            class="mb-4 w-auto max-w-[150px]"
           />
-          <h4 class="text-2xl font-bold text-white">{{ personName }}</h4>
-          <p class="text-gray-400 mt-2 font-light">{{ personTitle }}</p>
+          <h4 class="text-xl font-semibold text-[#161616]">{{ personName }}</h4>
+          <p class="text-[#636363] mt-2">{{ personTitle }}</p>
         </div>
-        
-        <!-- Right Column - Quote -->
-        <div class="flex flex-col justify-center relative z-10">
-          <i class="text-6xl fa-solid fa-quote-left mb-4" :class="quoteColor"></i>
-          <h3 class="text-3xl md:text-4xl font-bold text-white mb-4">
+        <!-- Right Column (2/3) -->
+        <div class="md:col-span-2 flex flex-col justify-center px-6">
+          <i class="text-6xl text-[#A620FF] fa-solid fa-quote-left"></i>
+          <h3 class="text-3xl font-bold text-[#161616] mt-4 mb-6">
             {{ quoteTitle }}
           </h3>
-          <p class="text-gray-300 mb-6 leading-relaxed text-lg">
-            {{ quoteText }}
-          </p>
+          <p class="text-[#636363]">"{{ quoteText }}"</p>
           <a
             :href="ctaLink"
             :class="`border border-white text-white px-6 py-2 font-semibold w-fit hover:bg-white hover:text-[#161616] transition-all duration-300`"
