588ba94602a221b0749c7209be6e7457e10306b3
sitesynth <hello@sitesynth.com>
2025-11-19 00:57:48 +0100

    Update DesktopMenu.vue

--- components/layout/DesktopMenu.vue
+++ components/layout/DesktopMenu.vue
@@ -7,10 +7,9 @@
           :href="item.link"
           :target="item.target"
 :class="[
-  'font-medium flex items-center space-x-3 transition-colors duration-200 hover:text-[#d9d9d9]',
+  'font-medium flex items-center space-x-3 transition-colors duration-200 hover:text-[rgb(217,217,217)]',
   isCurrentParent(item) ? 'current' : ''
 ]"
-
           <span :class="isCurrentParent(item) ? 'underline' : ''">
             {{ item.label }}
           </span>
