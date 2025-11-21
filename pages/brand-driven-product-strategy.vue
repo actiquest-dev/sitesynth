f3b6772c2bdda8a9dbd37e60b9e3be343e679f7c
sitesynth <hello@sitesynth.com>
2025-11-21 19:09:45 +0100

    Update brand-driven-product-strategy.vue

--- pages/brand-driven-product-strategy.vue
+++ pages/brand-driven-product-strategy.vue
@@ -63,7 +63,7 @@
 
   <Testimonial
     id="testimonial-mayya"
-    imageSrc="/assets/new-assets/people-for-CTA/Mayya.png"
+    imageSrc="/assets/new-assets/people-for-CTA/Mayya v2.png"
     personName="Mayya Aprosina"
     personTitle="CEO & Business Designer"
     quoteTitle="A clear why shapes everything that follows."
