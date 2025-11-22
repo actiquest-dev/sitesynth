875747a999f3e563fb5461355230e2543528d501
sitesynth <hello@sitesynth.com>
2025-11-22 22:54:26 +0100

    Update full-stack-implementation.vue

--- pages/full-stack-implementation.vue
+++ pages/full-stack-implementation.vue
@@ -60,7 +60,7 @@
   />
   <TestimonialWhite
     id="testimonial-marco"
-    imageSrc="assets/new-assets/people-for-CTA/Marco.png"
+    imageSrc="/assets/new-assets/people-for-CTA/oval-marco.png"
     personName="Marco Maffei"
     personTitle="Full-Stack Engineer"
     quoteTitle="Ideas are cheap. Shipping is rare. We focus on the rare."
