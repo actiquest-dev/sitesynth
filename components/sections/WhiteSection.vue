d8d709e4f82130980c1cb203d121f7e9974100e6
sitesynth <hello@sitesynth.com>
2025-12-07 21:27:50 +0100

    Update WhiteSection.vue

--- components/sections/WhiteSection.vue
+++ components/sections/WhiteSection.vue
@@ -15,8 +15,7 @@
         <div
           class="absolute inset-0 pointer-events-none opacity-100"
           :style="{
-            backgroundImage:
-              'url(/assets/gradients/gradient-for-banner-section.svg)',
+            backgroundImage: `url(${gradientImage})`,
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'cover',
@@ -30,15 +29,13 @@
           <div class="flex flex-col sm:flex-row justify-center gap-4 mt-6">
             <a
               :href="primaryLink"
-              :style="primaryButtonStyle"
-              class="px-6 py-2 border border-white bg-white text-[#161616] transition-colors duration-1000 font-semibold"
+              :class="`px-6 py-2 border border-white bg-white text-[#161616] transition-colors duration-1000 font-semibold hover:bg-[${hover}] hover:border-[${hover}] hover:text-white`"
             >
               {{ primaryText }}
             </a>
             <a
               :href="secondaryLink"
-              :style="secondaryButtonStyle"
-              class="px-6 py-2 border bg-[#161616] text-white transition-colors duration-1000 font-semibold"
+              :class="`px-6 py-2 border border-[${hover}] bg-[#161616] text-white transition-colors duration-1000 font-semibold hover:bg-[${hover}] hover:border-[${hover}] hover:text-white`"
             >
               {{ secondaryText }}
             </a>
@@ -83,6 +80,10 @@ const props = defineProps({
     type: String,
     default: null,
   },
+  gradientImage: {
+    type: String,
+    default: "/assets/gradients/gradient-for-banner-section.svg",
+  },
   hover: {
     type: String,
     default: "#8D35FF",
@@ -105,16 +106,4 @@ const backgroundImageStyle = computed(() => {
   }
   return {};
 });
-
-// Primary button style
-const primaryButtonStyle = computed(() => ({
-  borderColor: props.hover,
-  backgroundColor: props.hover,
-  color: "white",
-}));
-
-// Secondary button style
-const secondaryButtonStyle = computed(() => ({
-  borderColor: props.hover,
-}));
 </script>
