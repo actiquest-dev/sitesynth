01eaf9e3f5e41967153d88d4736561085278c9e4
Marcoatisabel <orsacchiotto80@hotmail.com>
2025-12-09 19:30:14 +0100

    Update favicon files and Tailwind config formatting
    Replaced favicon.ico and favicon.svg with new versions, including a resized SVG. Improved formatting in tailwind.config.js by switching to double quotes, adding trailing commas, and ensuring proper file ending.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
-    "./app.vue"
+    "./app.vue",
   ],
   theme: {
     extend: {},
@@ -14,14 +14,14 @@ module.exports = {
   plugins: [],
   safelist: [
     // Safelist hover colors to ensure Tailwind generates the classes
-    'hover:bg-[#0033ff]',
-    'hover:border-[#0033ff]',
-    'border-[#0033ff]',
-    'hover:bg-[#AA3733]',
-    'hover:border-[#AA3733]',
-    'border-[#AA3733]',
-    'hover:bg-[#8D35FF]',
-    'hover:border-[#8D35FF]',
-    'border-[#8D35FF]',
-  ]
-}
+    "hover:bg-[#0033ff]",
+    "hover:border-[#0033ff]",
+    "border-[#0033ff]",
+    "hover:bg-[#AA3733]",
+    "hover:border-[#AA3733]",
+    "border-[#AA3733]",
+    "hover:bg-[#8D35FF]",
+    "hover:border-[#8D35FF]",
+    "border-[#8D35FF]",
+  ],
+};
