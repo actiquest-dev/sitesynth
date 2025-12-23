11752500290f0ddaac6aee411475ffe39b14fcfa
sitesynth <hello@sitesynth.com>
2025-12-24 00:46:43 +0100

    Update JobOpportunity.vue

--- components/sections/JobOpportunity.vue
+++ components/sections/JobOpportunity.vue
@@ -63,11 +63,11 @@
             </div>
 
             <!-- LEFT IMAGE ZONE (NO padding, flush left) -->
-            <div class="h-full w-[260px] flex items-stretch">
+            <div class="h-full w-[260px] flex items-center justify-center">
               <img
                 :src="job.image"
                 :alt="job.imageAlt || job.title"
-                class="h-full w-full object-cover opacity-25"
+                class="h-[70%] w-auto object-contain opacity-25"
               />
             </div>
 
