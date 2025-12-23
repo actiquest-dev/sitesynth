bf96aced8e71a68349b9f7fad8b91ee350e77f08
sitesynth <hello@sitesynth.com>
2025-12-23 21:52:44 +0100

    Update JobOpportunity.vue

--- components/sections/JobOpportunity.vue
+++ components/sections/JobOpportunity.vue
@@ -2,14 +2,21 @@
   <section :id="id || undefined" :class="backgroundColor">
     <div class="max-w-[1248px] mx-auto px-6">
       <div class="space-y-12">
+
         <div
           v-for="(job, index) in jobs"
           :key="index"
           :class="[job.bgColor || '']"
-          class="job-box group relative border border-[#636363] flex cursor-pointer"
+          class="job-box group relative border border-[#636363]
+                 flex cursor-pointer
+                 min-h-[180px] md:min-h-[200px]"
         >
-          <a :href="job.link" class="flex w-full" rel="noopener noreferrer">
-            <!-- Image Section (1/6 width) -->
+          <a
+            :href="job.link"
+            class="flex w-full h-full no-underline"
+            rel="noopener noreferrer"
+          >
+            <!-- Image Section (1/6) -->
             <div class="w-1/6 flex-shrink-0">
               <img
                 :src="job.image"
@@ -18,43 +25,46 @@
               />
             </div>
 
-            <!-- Content Section (4/6 width) -->
+            <!-- Content Section (4/6) -->
             <div class="w-4/6 p-6 flex flex-col justify-center">
-              <!-- Title -->
-              <component :is="job.titleTag || 'h3'" :class="job.titleClass">
+              <component
+                :is="job.titleTag || 'h3'"
+                :class="job.titleClass"
+              >
                 {{ job.title }}
               </component>
 
-              <component
-                :is="job.descriptionClass ? 'p' : 'div'"
-                :class="job.descriptionClass"
-              >
+              <p :class="job.descriptionClass">
                 {{ job.description }}
-              </component>
+              </p>
             </div>
 
-            <!-- Link Section (1/6 width) -->
-            <div class="w-1/6 flex flex-col justify-center items-center">
+            <!-- Link Section (1/6) -->
+            <div class="w-1/6 flex items-center justify-center">
               <span
-                class="text-[#636363] flex items-center group-hover:text-white transition-colors duration-200"
+                class="text-[#636363] flex items-center
+                       group-hover:text-white transition-colors duration-200"
               >
                 <span class="mr-2 font-semibold">
                   {{ job.linkText }}
                 </span>
                 <i
-                  class="fa fa-chevron-right group-hover:translate-x-1 transition-transform duration-200"
+                  class="fa fa-chevron-right
+                         transition-transform duration-200
+                         group-hover:translate-x-1"
                 ></i>
               </span>
             </div>
           </a>
         </div>
+
       </div>
     </div>
   </section>
 </template>
 
 <script setup>
-const props = defineProps({
+defineProps({
   id: {
     type: String,
     default: "",
