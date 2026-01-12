4be05fdf755632c743626a042c9a7d549f2e600a
sitesynth <hello@sitesynth.com>
2026-01-12 14:16:06 +0100

    Update ai-innovation.vue

--- pages/ai-innovation.vue
+++ pages/ai-innovation.vue
@@ -148,7 +148,7 @@
     }"
     :rightContent="{
       image: {
-        src: '/assets/new-assets/solutions-page/solutions/ai-infused-innovation-and-rd/cta/membria-image.png',
+        src: '/assets/new-assets/solutions-page/solutions/ai-infused-innovation-and-rd/cta/membria-image.webp',
         alt: 'Membria Screenshot',
         class: 'w-full h-auto rounded-lg',
       },
