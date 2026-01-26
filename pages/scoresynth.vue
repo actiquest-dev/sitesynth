1e687432cc19961fb4058f573c383d0a52804b1b
sitesynth <hello@sitesynth.com>
2026-01-26 17:41:00 +0100

    Update scoresynth.vue

--- pages/scoresynth.vue
+++ pages/scoresynth.vue
@@ -12,7 +12,7 @@
       alt: 'ScoreSynth Logo',
       class: 'h-20 w-auto',
     }"
-    paddingClasses="flex items-center justify-center h-[100vh]"
+     paddingClasses="flex items-center justify-center h-[100vh] pt-[80px]"
     :content="[
       { tag: 'h1', text: 'ScoreSynth', margin: 'mb-6' },
       {
